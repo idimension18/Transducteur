90dbde55bb07c306189304d4263b3d60723de7d7
idimension <41388029+idimension18@users.noreply.github.com>
2024-03-28 22:11:15 +0100

    La concaténation

--- transducteur.ml
+++ transducteur.ml
@@ -11,14 +11,35 @@ let list_of_string chaine = List.rev (String.fold_left (fun acc x -> x::acc) [] 
 *)
 module Transducteur =
 	struct
-		type transi_uplet = int * char * string * int
+		type transi_uplet = int * char * char * int
 		type t = Transducteur of int * int list * transi_uplet list
 
+		
+		(* Boite à outils *)
+		(* -------------------- *)
+
+		(* Calcule l'indice maximale du transducteur 
+			Cela peut être utile pour rajouter des états *)
+		let q_max t = let Transducteur(_, _, transi_list) = t in
+			List.fold_left 
+				(fun acc x -> 
+					let (q1, _, _, q2) = x in 
+					if q1 > q2 then
+						if q1 > acc then q1
+						else acc
+					else
+						if q2 > acc then q2
+						else acc) 
+					0 transi_list 
+
+		
+		(* Fonctions d'éxécution *)
+		(* -------------------------------- *)
+		
 		(* fonction de type : string -> string 
 			Réécrit le mot passé en paramêtre et renvoit la réécriture *)
 		let exec transduct mot =
 			let Transducteur(debut, fins, transi_uplets) = transduct in
-
 			(* Fonction de transition  mu : Q X A -> Q X A 
 				Prends un état d'entrée et une lettre 
 				Renvoit l'état suivant (si existe) et la réécriture *)
@@ -37,20 +58,58 @@ module Transducteur =
 			let rec aux q entree sortie = 
 				match entree with 
 				| [] -> if in_list q fins then sortie else failwith "Mot non reconnu !"
-				| t::queue -> let (next_q, r) = mu q t in aux next_q queue (sortie^r)
+				| t::queue -> let (next_q, r) = mu q t in aux next_q queue (sortie^(Char.escaped r))
 			in
 
 			aux debut (list_of_string mot) ""
 
-			(* Fonction de type : t->t->string->string 
-				Calculs la composition : (t2 o t1) entree *)
-			let compose t2 t1 entree = exec t2 (exec t1 entree)
-	end
+		
+		(* Construction de nouveaux transducteurs *)
+		(* ---------------------------------------- *)
+		
+		(* Concatenation de deux transducteur t1 et t2 *)
+		let concac t1 t2 =
+			let Transducteur(s1, fin1, transi_list1) = t1 in
+			
+			(* fonction de type : int -> t -> t
+				Afin de concaténer t2 il faut rennomer ces états à partir du max de t1 *)
+			let re_indice base t = let Transducteur(s, f, transis) = t in
+				Transducteur(s + base, 
+					List.fold_right (fun x acc -> (x+base)::acc) f [],
+					List.fold_right (fun x acc -> let (q1,l,e,q2) = x in (q1+base,l,e,q2+base)::acc) transis [])
+			in
 
-(*
-module Automate =
-	struct
-		type 'a transition = ('a * string * 'a)
-		type 'a t = Automate of ('a * ('a transition) list)
+			(* Fonction de type : int list -> int -> transi_uplet list
+				On branche la fin de t1 sur le debut de t2 *)
+			let branchage fins1 s2 transis2 = 
+				List.fold_right
+					( fun x acc -> let (q1, l, e, q2) = x in
+						if q1 <> s2 then x::acc
+						else
+							(List.fold_right (fun fin1 acc_fins -> (fin1, l, e, q2)::acc_fins) fins1 []) @ acc )
+					transis2 []
+			in
+			
+			let Transducteur(s1, fin1, transis1) = t1 in
+			let Transducteur(s2, fin2, transis2) = re_indice ((q_max t1)+1) t2 in
+			Transducteur(s1, fin2, transis1 @ (branchage fin1 s2 transis2) )
+		
+		
+		let kleene t = ()
+
+		
+		(* Application divers *)
+		(* -------------------- *)
+		
+		(* Fonction de type : t -> t -> string -> string 
+			Calculs la composition : (t2 o t1) entree *)
+		let compose t1 t2 entree = exec t2 (exec t1 entree)
+
+
+		(* Calcule l'automate reconnaissant le langage d'entrée *)
+		let projection_gauche = ()
+
+		
+		(* Calcule l'automate reconnaison le langage de sortie *)
+		let projection_droite = ()
 	end
-*) 
